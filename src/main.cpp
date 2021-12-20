316f1b5405f9741d4f8827719172fd316882f82a
Pesteves2002 <tomasesteves2002@gmail.com>
2021-12-20 12:54:09 +0000

    Propose by Pesteves
    Co-authored-by: Diogo Correia <me@diogotc.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 void fill_vector_until_newline(std::vector<std::int64_t> *sequence);
 void solve_p1(std::vector<std::int64_t> *sequence, std::int64_t *result);
+void solve_p1_aux(std::vector<std::int64_t> *sequence, std::int64_t *result, int index, int len, int curr_num);
+
 int64_t solve_p2(std::vector<std::int64_t> *sequence1,
                  std::vector<std::int64_t> *sequence2);
 
@@ -44,9 +47,42 @@ void fill_vector_until_newline(std::vector<std::int64_t> *sequence)
 
 void solve_p1(std::vector<std::int64_t> *sequence, std::int64_t *result)
 {
-    // TODO
-    result[0] = 69;
-    result[1] = 420;
+    int seq_len = sequence->size();
+    result[0] = sequence->at(0);
+    int curr_num = std::numeric_limits<int>::min();
+
+    for (int index = 0; index < seq_len; index++)
+    {
+        solve_p1_aux(sequence, result, index, 0, curr_num);
+    }
+}
+
+void solve_p1_aux(std::vector<std::int64_t> *sequence, std::int64_t *result, int index, int len, int curr_num)
+{
+    if (index + 1 == (int)sequence->size())
+    {
+        std::cout << len << std::endl;
+        return;
+    }
+
+    if (sequence->at(index + 1) > curr_num)
+    {
+        // accepts the next number
+        solve_p1_aux(sequence, result, index + 1, len + 1, sequence->at(index + 1));
+        // skips the next number
+        solve_p1_aux(sequence, result, index + 1, len, curr_num);
+    }
+    else
+    {
+        if (sequence->at(index + 1) == curr_num)
+        {
+            solve_p1_aux(sequence, result, index + 1, len, curr_num + 1);
+        }
+        else
+        {
+            solve_p1_aux(sequence, result, index + 1, len, std::numeric_limits<int>::min());
+        }
+    }
 }
 
 std::int64_t solve_p2(std::vector<std::int64_t> *sequence1,
